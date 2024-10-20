5c336b5afc0fec1559d92910b181c2f12673ae02
Lily <egzozu.be.bas@gmail.com>
2024-10-20 12:48:11 +0300

    add new hint type

--- source/mobile/objects/Hitbox.hx
+++ source/mobile/objects/Hitbox.hx
@@ -169,7 +169,25 @@ class Hitbox extends MobileInputManager implements IMobileControls
 		var shape:Shape = new Shape();
 		shape.graphics.beginFill(0xFFFFFF);
 
-		if (ClientPrefs.data.hitboxType == 'Gradient')
+		if (ClientPrefs.data.hitboxType == "No Gradient")
+		{
+			var matrix:Matrix = new Matrix();
+			matrix.createGradientBox(Width, Height, 0, 0, 0);
+
+			if (isLane)
+				shape.graphics.beginFill(0xFFFFFF);
+			else
+				shape.graphics.beginGradientFill(RADIAL, [0xFFFFFF, 0xFFFFFF], [0, 1], [60, 255], matrix, PAD, RGB, 0);
+			shape.graphics.drawRect(0, 0, Width, Height);
+			shape.graphics.endFill();
+		}
+		else if (ClientPrefs.data.hitboxType == "No Gradient (Old)")
+		{
+			shape.graphics.lineStyle(10, 0xFFFFFF, 1);
+			shape.graphics.drawRect(0, 0, Width, Height);
+			shape.graphics.endFill();
+		}
+		else // if (ClientPrefs.data.hitboxType == 'Gradient')
 		{
 			shape.graphics.lineStyle(3, 0xFFFFFF, 1);
 			shape.graphics.drawRect(0, 0, Width, Height);
@@ -181,12 +199,6 @@ class Hitbox extends MobileInputManager implements IMobileControls
 			else
 				shape.graphics.beginGradientFill(RADIAL, [0xFFFFFF, FlxColor.TRANSPARENT], [1, 0], [0, 255], null, null, null, 0.5);
 			shape.graphics.drawRect(3, 3, Width - 6, Height - 6);
-			shape.graphics.endFill();
-		}
-		else
-		{
-			shape.graphics.lineStyle(10, 0xFFFFFF, 1);
-			shape.graphics.drawRect(0, 0, Width, Height);
 			shape.graphics.endFill();
 		}
 
