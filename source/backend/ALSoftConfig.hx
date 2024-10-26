2df3d76e771abae215a72bb3431db9173fd91952
Lily <egzozu.be.bas@gmail.com>
2024-10-26 14:19:33 +0300

    reverting karim's changes cuz `__init__` is too early for openfl and lime

--- source/backend/ALSoftConfig.hx
+++ source/backend/ALSoftConfig.hx
@@ -1,10 +1,6 @@
 package backend;
 
 import haxe.io.Path;
-import lime.system.System;
-#if android
-import lime.system.JNI;
-#end
 
 /*
 A class that simply points OpenALSoft to a custom configuration file when the game starts up.
@@ -15,34 +11,18 @@ The config overrides a few global OpenALSoft settings with the aim of improving 
 	#if desktop
 	static function __init__():Void
 	{
-		var origin:String = #if hl Sys.getCwd() #elseif mobile Path.addTrailingSlash(System.applicationStorageDirectory) #else Sys.programPath() #end;
+		var origin:String = #if hl Sys.getCwd() #else Sys.programPath() #end;
 
 		var configPath:String = Path.directory(Path.withoutExtension(origin));
 		#if windows
 		configPath += "/plugins/alsoft.ini";
 		#elseif mac
 		configPath = Path.directory(configPath) + "/Resources/plugins/alsoft.conf";
-		#elseif android
-		configPath = origin + 'alsoft.conf';
-		var exists:Bool = FileSystem.exists(configPath);
-		if (!exists) File.saveContent(configPath, openfl.Assets.getText('alsoft.conf'));
-		exists = FileSystem.exists(configPath);
 		#else
 		configPath += "/plugins/alsoft.conf";
 		#end
 
-		#if android
-		if (exists)
-		{
-			JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'nativeSetenv', '(Ljava/lang/String;Ljava/lang/String;)V')("ALSOFT_CONF", configPath);
-		}
-		else
-		{
-			trace("Couldn't configure ALSoft because the the config file doesn't exist on the current working directory.");
-		}
-		#else
 		Sys.putEnv("ALSOFT_CONF", configPath);
-		#end
 	}
 	#end
 }
