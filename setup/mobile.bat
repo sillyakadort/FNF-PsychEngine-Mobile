285315893390207286b40fe44b46b0765c382cf0
Lily <egzozu.be.bas@gmail.com>
2025-01-12 16:31:22 +0300

    forgor

--- setup/mobile.bat
+++ setup/mobile.bat
@@ -10,7 +10,7 @@ haxelib install openfl 9.3.3 --quiet
 haxelib git flixel https://github.com/MobilePorting/flixel 5.6.1 --quiet
 haxelib install flixel-addons 3.2.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
-haxelib install hscript-iris 1.1.0 --quiet
+haxelib install hscript-iris 1.1.3 --quiet
 haxelib install tjson 1.4.0 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
 haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit-0.7plus --quiet
